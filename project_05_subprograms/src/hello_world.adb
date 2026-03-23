bb69588ca7f13065ff0d2ec26d553b0184f0087b
Hyun Lee <wildriver1105@gmail.com>
2026-03-23 21:23:27 +0900

    Subprograms written

---
+++ project_05_subprograms/src/hello_world.adb
@@ -0,0 +1,4 @@
+function hello_world return String is
+begin
+   return "Hello, World!";
+end hello_world;
---
+++ project_05_subprograms/src/project_05_subprograms.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO;
+with hello_world;
+
+procedure Project_05_Subprograms is
+begin
+   Ada.Text_IO.Put_Line(hello_world);
+end Project_05_Subprograms;
